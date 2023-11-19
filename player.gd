a7cad61a273902a1c589dae638bfac70945affbc
Ryan Lay <30853424+totally-not-frito-lays@users.noreply.github.com>
2023-11-18 22:03:25 -0800

    finished part 1

--- player.gd
+++ player.gd
@@ -1,30 +1,49 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
-const ACCELERATION = 600.0
-const FRICTION = 600.0
+const ACCELERATION = 800.0
+const FRICTION = 1000.0
 const JUMP_VELOCITY = -300.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 # Runs x ticks every second
 func _physics_process(delta):
-	# Add the gravity.
+	apply_gravity(delta)	
+	handle_jump()
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	handle_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
+	update_animations(input_axis)
+	move_and_slide()
+
+func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
 	
-	if direction != 0:
-		# adds inertia to movement. Acceleration is not handled by move_and_slide()
-		velocity.x = move_toward(velocity.x, SPEED * direction, ACCELERATION * delta)
+func handle_jump():
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 	else:
+		# Handles different jump heights
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_VELOCITY / 2:
+			velocity.y = JUMP_VELOCITY / 2
+
+func handle_acceleration(input_axis, delta):
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
+	
+func apply_friction(input_axis, delta):
+	if input_axis == 0:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 
-	# moves character by velocity, sliding on static bodies
-	move_and_slide()
+func update_animations(input_axis):
+	if input_axis != 0:
+		animated_sprite_2d.flip_h = (input_axis < 0)
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
--- world.gd
+++ world.gd
@@ -4,5 +4,5 @@ extends Node2D
 @onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
 
 func _ready():
-	# Assigns collision shape with polygon
+	RenderingServer.set_default_clear_color(Color.BLACK)
 	polygon_2d.polygon = collision_polygon_2d.polygon
