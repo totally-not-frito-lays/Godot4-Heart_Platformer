6ccd62848cc88223f8d05187984cb73f4a4790c8
Ryan Lay <30853424+totally-not-frito-lays@users.noreply.github.com>
2023-11-20 23:35:18 -0800

    Part 2 - Coyote Jump
    - Tutorial jump has buggy condition
    - Needs to extract the coyote timer into separate condition check

--- player.gd
+++ player.gd
@@ -7,8 +7,8 @@ const JUMP_VELOCITY = -300.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
 
-# Runs x ticks every second
 func _physics_process(delta):
 	apply_gravity(delta)	
 	handle_jump()
@@ -16,7 +16,12 @@ func _physics_process(delta):
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
 	update_animations(input_axis)
+	
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
 
 func apply_gravity(delta):
 	if not is_on_floor():
@@ -26,7 +31,10 @@ func handle_jump():
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
-	else:
+	if coyote_jump_timer.time_left > 0.0:
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = JUMP_VELOCITY
+	if not is_on_floor():
 		# Handles different jump heights
 		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
